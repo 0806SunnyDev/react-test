ae9bf391ee584499641b9df38fc36d31329c8075
Sunny Dev <lightdev05@gmail.com>
2023-09-10 06:50:51 +0100

    FIX: update the login email validation

--- backend/validators/loginValidator.js
+++ backend/validators/loginValidator.js
@@ -2,6 +2,7 @@ const { check } = require('express-validator')
 
 const loginValidator = [
   check('email', 'Please include a valid email').isEmail(),
+  check('email', 'Email is required').exists(),
   check('password', 'Password is required').exists(),
 ]
 
