f007f4b06e09cddead50cb45af381be9ca48cdfd
shinesoftdev86 <lightdev05@gmail.com>
2024-09-17 23:15:16 -0400

    udpated the db url

--- backend/config/key.js
+++ backend/config/key.js
@@ -1,5 +1,5 @@
 module.exports = {
-  MONGO_URL: process.env.MONGO_URL || "mongodb://localhost:27017/test",
+  MONGO_URL: process.env.MONGO_URL || "mongodb://127.0.0.1:27017/test",
   SECRET_KEY: process.env.SECRET_KEY || "REACT_SKILL_TEST",
   APP_PORT: process.env.APP_PORT || 8000
 }
